aef97d7e28ace2d3421b28a77c6052dd32cb2779
ajoymaity <ajoy.maity@tarento.com>
2018-08-27 16:09:56 +0530

    duplicate number

---
+++ duplicate.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    vector<int>v(26);
+    for(int i=0;i<s.size();i++){
+       v[s[i]-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        if(v[i]!=0){
+        	cout<<(char)(i+'a')<<" is "<<v[i]<<" times "<<endl;
+        }
+    }
+    return 0;
+}
